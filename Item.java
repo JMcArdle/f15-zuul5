259c6a246ad9122a82f65480f875ba2311727ce8
ScottT4ylor <G00236985@W213-10.academic.raritanval.edu>
2015-11-02 21:49:49 -0500

    Created Item Class
    New class for items.

---
+++ Item.java
@@ -0,0 +1,36 @@
+
+/**
+ * This class is part of _____, a text based adventure game created
+ *  by modifying the "World of Zuul" game.
+ *  
+ *  It 
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Item
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Item
+     */
+    public Item()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
