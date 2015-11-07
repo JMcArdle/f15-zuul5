0d851f91f55a3320a87b5b5b93496a0025ad635d
ScottT4ylor <G00236985@smail.raritanval.edu>
2015-11-07 16:10:13 -0500

    Internal Commands
    Creating the internal commands. Finished movePlayer, moveNPC, moveItem,
    addNPC, removeNPC, addNPCTalk, removeNPCTalk, addItem, removeItem.

--- InternalCommandProcessor.java
+++ InternalCommandProcessor.java
@@ -69,6 +69,11 @@ public class InternalCommandProcessor
                     case "-rnpc":
                         goRemoveNPC();
                         break;
+                    case "-npcatalk":
+                        goAddNPCTalk();
+                        break;
+                    case "-npcrtalk":
+                        goRemoveNPCTalk();
                     case "-aitem":
                         goAddItem();
                         break;
@@ -175,7 +180,7 @@ public class InternalCommandProcessor
         }
         else
         {
-            System.out.println("Debug: the move npc command didn't have enough words, needs at least 3");
+            System.out.println("Debug: the move npc command didn't have enough words, needs at least 3 (command npc room)");
         }
     }
     
@@ -184,15 +189,260 @@ public class InternalCommandProcessor
     /////The methods I still need to finish//////
     
     private void goMoveItem()
-    {}
+    {
+        if(command.countTokens() >= 3)
+        {
+            String arg1 = command.nextToken();
+            String arg2 = command.nextToken();
+            if (arg2.equalsIgnoreCase("to"))
+            {
+                if(command.hasMoreTokens())
+                {
+                    arg2 = command.nextToken();
+                }
+                else
+                {
+                    System.out.println("Debug: the move player command didn't specify where to move");
+                }
+            }
+            Item itemToMove = resolver.resolveItemFromCurrentRoom(arg1);
+            if(itemToMove == null)
+            {
+                itemToMove = resolver.resolveItemFromGlobal(arg1);
+            }
+            if(itemToMove != null)
+            {
+                Room goTo = resolver.resolveRoom(arg2);
+                if (goTo != null)
+                {
+                    resolver.findItem(itemToMove).removeItem(itemToMove);
+                    goTo.addItem(itemToMove);
+                }
+                else
+                {
+                    System.out.println("Debug: that room couldn't be found.");
+                }
+            }
+            else
+            {
+                System.out.println("Debug: that item couldn't be found.");
+            }
+        }
+        else
+        {
+            System.out.println("Debug: the move item command didn't have enough words, needs at least 3 (command npc room)");
+        }
+    
+    }
+    
     private void goAddNPC()
-    {}
+    {
+        if(command.countTokens() == 12)
+        {
+            String name = command.nextToken();
+            String look = command.nextToken();
+            String attack = command.nextToken();
+            String leer = command.nextToken();
+            String lick = command.nextToken();
+            String torture = command.nextToken();
+            String escape = command.nextToken();
+            String listen = command.nextToken();
+            String dodge = command.nextToken();
+            String grab = command.nextToken();
+            String room = command.nextToken();
+            Room goTo = null;
+            goTo = resolver.resolveRoom(room);
+            if (goTo != null)
+            {
+                goTo.addNPC(new NPC(name,look,attack,leer,lick,torture,escape,listen,dodge,grab));
+            }
+            else
+            {
+                System.out.println("Debug: the room you want to add an an npc to doesn't exist");
+            }
+        }
+        else
+        {
+            System.out.println("Debug: addNPC internal command needs 12 words (command, 10 words for the signature, and the room name");
+        }
+    }
+    
     private void goRemoveNPC()
-    {}
+    {
+        if(command.countTokens() >= 3)
+        {
+            String arg1 = command.nextToken();
+            String arg2 = command.nextToken();
+            if (arg2.equalsIgnoreCase("from"))
+            {
+                if(command.hasMoreTokens())
+                {
+                    arg2 = command.nextToken();
+                }
+                else
+                {
+                    System.out.println("Debug: the remove npc command didn't specify where to remove from");
+                }
+            }
+            NPC npcToRemove = resolver.resolveNPCFromCurrentRoom(arg1);
+            if(npcToRemove == null)
+            {
+                npcToRemove = resolver.resolveNPCFromGlobal(arg1);
+            }
+            if(npcToRemove != null)
+            {
+                Room goTo = resolver.resolveRoom(arg2);
+                if (goTo != null)
+                {
+                    goTo.removeNPC(npcToRemove);
+                }
+                else
+                {
+                    System.out.println("Debug: that room couldn't be found.");
+                }
+            }
+            else
+            {
+                System.out.println("Debug: that npc couldn't be found.");
+            }
+        }
+        else
+        {
+            System.out.println("Debug: the remove npc command didn't have enough words, needs at least 3 (command npc room)");
+        }
+    }
+    
+    private void goAddNPCTalk()
+    {
+        if (command.countTokens() >= 3)
+        {
+            String arg1 = command.nextToken();
+            String arg2 = command.nextToken();
+            NPC targetNPC = resolver.resolveNPCFromCurrentRoom(arg1);
+            if(targetNPC == null)
+            {
+                targetNPC = resolver.resolveNPCFromGlobal(arg1);
+            }
+            if(targetNPC != null)
+            {
+                targetNPC.addTalk(arg1);
+            }
+            else
+            {
+                System.out.println("Debug: could not find the npc");
+            }
+        }
+        else
+        {
+            System.out.println("Debug add npc talk needs 3 words (command npc words)");
+        }
+    }
+    
+    private void goRemoveNPCTalk()
+    {
+        if (command.countTokens() >= 3)
+        {
+            String arg1 = command.nextToken();
+            String arg2 = command.nextToken();
+            NPC targetNPC = resolver.resolveNPCFromCurrentRoom(arg1);
+            if(targetNPC == null)
+            {
+                targetNPC = resolver.resolveNPCFromGlobal(arg1);
+            }
+            if(targetNPC != null)
+            {
+                targetNPC.removeTalk(arg1);
+            }
+            else
+            {
+                System.out.println("Debug: could not find the npc");
+            }
+        }
+        else
+        {
+            System.out.println("Debug remove npc talk needs 3 words (command npc words)");
+        }
+    }
+    
     private void goAddItem()
-    {}
+    {
+        if(command.countTokens() == 10)
+        {
+            String name = command.nextToken();
+            String look = command.nextToken();
+            String attack = command.nextToken();
+            String lick = command.nextToken();
+            String listen = command.nextToken();
+            String dodge = command.nextToken();
+            String grab = command.nextToken();
+            String eq = command.nextToken();
+            boolean equipable = false;
+            String room = command.nextToken();
+            Room goTo = null;
+            if (eq.equalsIgnoreCase("1") || eq.equalsIgnoreCase("01") || eq.equalsIgnoreCase("yes") || eq.equalsIgnoreCase("true"))
+            {
+                equipable = true;
+            }
+            goTo = resolver.resolveRoom(room);
+            if (goTo != null)
+            {
+                goTo.addItem(new Item(name,look,attack,lick,listen,dodge,grab, equipable));
+            }
+            else
+            {
+                System.out.println("Debug: the room you want to add an an item to doesn't exist");
+            }
+        }
+        else
+        {
+            System.out.println("Debug: addItem internal command needs 9 words (command, 9 words for the signature, and the room name");
+        }
+    }
+    
     private void goRemoveItem()
-    {}
+    {
+        if(command.countTokens() >= 3)
+        {
+            String arg1 = command.nextToken();
+            String arg2 = command.nextToken();
+            if (arg2.equalsIgnoreCase("from"))
+            {
+                if(command.hasMoreTokens())
+                {
+                    arg2 = command.nextToken();
+                }
+                else
+                {
+                    System.out.println("Debug: the remove item command didn't specify where to remove it from");
+                }
+            }
+            Item itemToRemove = resolver.resolveItemFromCurrentRoom(arg1);
+            if(itemToRemove == null)
+            {
+                itemToRemove = resolver.resolveItemFromGlobal(arg1);
+            }
+            if(itemToRemove != null)
+            {
+                Room goTo = resolver.resolveRoom(arg2);
+                if (goTo != null)
+                {
+                    goTo.removeItem(itemToRemove);
+                }
+                else
+                {
+                    System.out.println("Debug: that room couldn't be found.");
+                }
+            }
+            else
+            {
+                System.out.println("Debug: that item couldn't be found.");
+            }
+        }
+        else
+        {
+            System.out.println("Debug: the remove item command didn't have enough words, needs at least 3 (command item room)");
+        }
+    }
     private void goAddInventory()
     {}
     private void goRemoveInventory()
